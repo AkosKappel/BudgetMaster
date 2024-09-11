07727282cae781b32ccd56858396188ad3efa17a
Akos Kappel <akos.kappel@svarba.sk>
2024-09-11 21:49:07 +0200

    update initial timeline design

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,78 +1,103 @@
-// import { Transaction } from '@/types';
-
-type Transaction = {
-  id: number;
-  date: string;
-  title: string;
-  description: string;
-  amount: number;
-  isExpense: boolean;
-};
+import { Transaction, groupedTransactions, transactions } from '@/app/types';
 
 type TransactionsPageProps = {
   transactions: Transaction[];
 };
 
-const transactions: Transaction[] = [
-  {
-    id: 1,
-    date: '2024-01-01',
-    title: 'Salary',
-    description: 'Received salary for January',
-    amount: 5000,
-    isExpense: false,
-  },
-  {
-    id: 2,
-    date: '2024-01-05',
-    title: 'Groceries',
-    description: 'Purchased groceries',
-    amount: 200,
-    isExpense: true,
-  },
-  {
-    id: 3,
-    date: '2024-01-10',
-    title: 'Dinner',
-    description: 'Dinner with friends',
-    amount: 100,
-    isExpense: true,
-  },
-];
-
-const TransactionsPage: React.FC<TransactionsPageProps> = () => {
+const TransactionsPage: React.FC<TransactionsPageProps> = (props) => {
   return (
     <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-      {transactions.map((transaction, index) => (
-        <li key={transaction.id}>
+      {Object.entries(groupedTransactions).map(([date, transactions], index) => (
+        <li key={date}>
           <hr />
           <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className={`h-6 w-6 ${transaction.isExpense ? 'text-red-500' : 'text-green-500'}`}
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <time className="font-mono text-gray-500 italic block mb-1 mx-2">{date}</time>
           </div>
-          <div className={`timeline-${index % 2 === 0 ? 'start md:text-end' : 'end'} mb-10`}>
+          <div className="timeline-start md:text-end">
+            {transactions
+              .filter((t) => !t.isExpense)
+              .map((transaction: Transaction) => (
+                <div key={transaction.title} className="mb-4 flex justify-start md:justify-end">
+                  <div className="px-4 py-2 rounded-lg shadow-md bg-green-100 inline-block">
+                    <div className="text-lg font-semibold whitespace-nowrap flex justify-start md:justify-end items-center">
+                      <h3 className="text-gray-800 mr-3">{transaction.title}</h3>
+                      <span className="text-sm text-gray-600">+ ${transaction.amount.toFixed(2)}</span>
+                    </div>
+                    <div className="text-gray-700 mt-1">{transaction.description}</div>
+                    <div className="text-sm text-gray-600 mt-2">
+                      {transaction.sender && <div>From: {transaction.sender}</div>}
+                      {transaction.receiver && <div>To: {transaction.receiver}</div>}
+                    </div>
+                    {transaction.labels.length > 0 && (
+                      <div className="flex flex-wrap mt-2 justify-start md:justify-end gap-1">
+                        {transaction.labels.map((label, i) => (
+                          <span key={i} className="bg-green-200 text-green-800 text-xs px-2 py-1 rounded">
+                            {label}
+                          </span>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              ))}
+          </div>
+          <div className="timeline-end">
+            {transactions
+              .filter((t) => t.isExpense)
+              .map((transaction: Transaction) => (
+                <div key={transaction.title} className="mb-4 flex justify-start">
+                  <div className="px-4 py-2 rounded-lg shadow-md bg-red-100 inline-block">
+                    <div className="text-lg font-semibold whitespace-nowrap flex justify-start items-center">
+                      <h3 className="text-gray-800 mr-3">{transaction.title}</h3>
+                      <span className="text-sm text-gray-600">- ${transaction.amount.toFixed(2)}</span>
+                    </div>
+                    <div className="text-gray-700 mt-1">{transaction.description}</div>
+                    <div className="text-sm text-gray-600 mt-2">
+                      {transaction.sender && <div>From: {transaction.sender}</div>}
+                      {transaction.receiver && <div>To: {transaction.receiver}</div>}
+                    </div>
+                    {transaction.labels.length > 0 && (
+                      <div className="flex flex-wrap mt-2 justify-start gap-1">
+                        {transaction.labels.map((label, i) => (
+                          <span key={i} className="bg-red-200 text-red-800 text-xs px-2 py-1 rounded">
+                            {label}
+                          </span>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              ))}
+          </div>
+          <hr className="my-8" />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+{
+  /* {transactions.map((transaction: Transaction, index: number) => (
+        <li key={transaction.title}>
+          <hr />
+          <div className={`timeline-${!transaction.isExpense ? 'start md:text-end' : 'end'} mb-10`}></div>
+          <div className="timeline-middle">
             <time className="font-mono text-gray-500 italic block mb-1">
               {new Date(transaction.date).toLocaleDateString()}
             </time>
+          </div>
+          <div className={`timeline-${transaction.isExpense ? 'start md:text-end' : 'end'} mb-10`}>
             <div className={`px-4 py-2 rounded-lg shadow-md ${transaction.isExpense ? 'bg-red-100' : 'bg-green-100'}`}>
-              <div className="text-lg font-semibold">{transaction.title}</div>
+              <div className="text-lg font-semibold">
+                <h3 className="text-gray-800">{transaction.title}</h3>
+                <span className="text-sm text-gray-600">
+                  {transaction.isExpense ? '-' : '+'} ${transaction.amount.toFixed(2)}
+                </span>
+              </div>
               <p className="text-gray-700 mt-1">{transaction.description}</p>
               <div className="flex justify-between items-center mt-2">
-                <span className="text-sm text-gray-600">
-                  <strong>Amount:</strong> ${transaction.amount.toFixed(2)}
-                </span>
                 <span
-                  className={`inline-block px-2 py-1 text-sm font-bold rounded-full ${
+                  className={`inline-block px-2 py-1 mx-2 text-sm font-bold rounded-full ${
                     transaction.isExpense ? 'bg-red-500 text-white' : 'bg-green-500 text-white'
                   }`}
                 >
@@ -83,9 +108,10 @@ const TransactionsPage: React.FC<TransactionsPageProps> = () => {
           </div>
           {index !== transactions.length - 1 && <hr />}
         </li>
-      ))}
-    </ul>
-  );
-};
+      ))} */
+}
+//     </ul>
+//   );
+// };
 
 export default TransactionsPage;
---
+++ app/types.ts
@@ -0,0 +1,141 @@
+export type Transaction = {
+  title: string;
+  date: string;
+  isExpense: boolean;
+  amount: number;
+  description: string;
+  labels: string[];
+  sender?: string | null;
+  receiver?: string | null;
+};
+
+// TODO: delete dummy data
+export const transactions: Transaction[] = [
+  {
+    title: 'Grocery Shopping',
+    date: '2023-09-01',
+    isExpense: true,
+    amount: 50.75,
+    description: 'Bought groceries from the local market',
+    labels: ['Food', 'Shopping'],
+    sender: null,
+    receiver: 'Supermarket',
+  },
+  {
+    title: 'Salary',
+    date: '2023-09-01',
+    isExpense: false,
+    amount: 2500,
+    description: 'Received salary for September',
+    labels: ['Income', 'Work'],
+    sender: 'Company ABC',
+    receiver: null,
+  },
+  {
+    title: 'Salary',
+    date: '2024-01-01',
+    isExpense: false,
+    amount: 1000,
+    description: 'Monthly salary',
+    labels: ['Income', 'Salary'],
+    sender: null,
+    receiver: 'John Doe',
+  },
+  {
+    title: 'Rent',
+    date: '2024-01-02',
+    isExpense: true,
+    amount: 1000,
+    description: 'Monthly rent',
+    labels: ['Expense', 'Rent'],
+    sender: 'John Doe',
+    receiver: null,
+  },
+  {
+    title: 'Groceries',
+    date: '2024-01-03',
+    isExpense: true,
+    amount: 100,
+    description: 'Weekly groceries',
+    labels: ['Expense', 'Groceries'],
+    sender: 'John Doe',
+    receiver: null,
+  },
+  {
+    title: 'Dinner',
+    date: '2024-01-04',
+    isExpense: true,
+    amount: 50.99,
+    description: 'Dinner with friends at Mcdonalds',
+    labels: ['Expense', 'Dinner', 'Restaurant', 'Food', 'Friends'],
+    sender: 'John Doe',
+    receiver: 'Restaurant',
+  },
+  {
+    title: 'Breakfast',
+    date: '2024-01-04',
+    isExpense: true,
+    amount: 30.25,
+    description: '',
+    labels: ['Expense', 'Breakfast'],
+    sender: 'John Doe',
+    receiver: 'Cafe',
+  },
+  {
+    title: 'Luck',
+    date: '2024-01-04',
+    isExpense: false,
+    amount: 1.0,
+    description: 'Found $1 on the street',
+    labels: [],
+    sender: null,
+    receiver: null,
+  },
+  {
+    title: 'Salary',
+    date: '2024-01-06',
+    isExpense: false,
+    amount: 1500,
+    description: '',
+    labels: ['Salary', 'Work'],
+  },
+  {
+    title: 'Bonus',
+    date: '2024-01-06',
+    isExpense: false,
+    amount: 50,
+    description: 'Extra bonus for the project, received from the client. Thank you!',
+    labels: ['Work'],
+  },
+  {
+    title: 'Lunch',
+    date: '2024-01-06',
+    isExpense: true,
+    amount: 150,
+    description: "Lunch with friends at Sam's",
+    labels: ['Expense', 'Lunch'],
+    sender: 'John Doe',
+    receiver: null,
+  },
+  {
+    title: 'Lottery',
+    date: '2024-01-06',
+    isExpense: false,
+    amount: 1000,
+    description: 'Win a small amount of money from a local lottery',
+    labels: [],
+    sender: '',
+    receiver: 'Me',
+  },
+].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+export const groupBy = (key: keyof Transaction, transactions: Transaction[]) => {
+  return transactions.reduce((acc: { [key: string]: Transaction[] }, transaction) => {
+    const groupKey = transaction[key]?.toString() ?? 'unknown';
+    if (!acc[groupKey]) acc[groupKey] = [];
+    acc[groupKey].push(transaction);
+    return acc;
+  }, {});
+};
+
+export const groupedTransactions = groupBy('date', transactions);
