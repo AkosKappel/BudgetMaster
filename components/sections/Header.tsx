582b075275a7c9a694f22fff7c12b4684f5d9534
Akos Kappel <akos.kappel@svarba.sk>
2024-11-10 13:50:17 +0100

    add sign up/ log in buttons

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -4,22 +4,28 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-primary text-primary-content shadow-lg">
-      <div className="mx-auto p-4 flex items-center justify-between">
-        <Link href="/" className="flex items-center space-x-4">
-          <Image
-            src="/logo.jpg"
-            alt="Budget Master Logo"
-            width={50}
-            height={50}
-            className="rounded-full"
-          />
-          <div>
-            <h1 className="text-3xl font-extrabold">Budget Master</h1>
-            <p className="text-sm italic">Master Your Money, Master Your Life</p>
-          </div>
-        </Link>
-      </div>
+    <header className="bg-primary text-primary-content shadow-lg flex justify-between">
+      <Link href="/" className="flex items-center m-4">
+        <Image
+          src="/logo.jpg"
+          alt="Budget Master Logo"
+          width={50}
+          height={50}
+          className="rounded-full mr-2"
+        />
+        <div>
+          <h1 className="text-3xl font-extrabold">Budget Master</h1>
+          <p className="text-sm italic">Master Your Money, Master Your Life</p>
+        </div>
+      </Link>
+      <ul className="flex items-center gap-4 m-4">
+        <li className="cursor-pointer rounded-full px-4 py-2 bg-white font-semibold hover:scale-105 transition duration-300">
+          <Link href="/">Sign Up</Link>
+        </li>
+        <li className="cursor-pointer rounded-full px-4 py-2 bg-white font-semibold hover:scale-105 transition duration-300">
+          <Link href="/">Log in</Link>
+        </li>
+      </ul>
     </header>
   );
 };
