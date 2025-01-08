1f2a70e86aabfdfbb48f29056cc936c3af5ead36
Akos Kappel <akos.kappel@svarba.sk>
2025-01-08 23:26:35 +0100

    dropdown options bugfix

--- store/transactionsSlice.ts
+++ store/transactionsSlice.ts
@@ -17,22 +17,14 @@ const initialState: TransactionsState = {
 
 const updateUniqueCategories = (state: TransactionsState) => {
   state.existingCategories = Array.from(
-    new Set(
-      ...state.existingCategories,
-      state.transactions.map((t) => t.category),
-    ),
+    new Set([...state.existingCategories, ...state.transactions.map((t) => t.category)]),
   )
     .map((c) => c.toUpperCase())
     .sort((a, b) => a.localeCompare(b));
 };
 
 const updateUniqueLabels = (state: TransactionsState) => {
-  state.existingLabels = Array.from(
-    new Set(
-      ...state.existingLabels,
-      state.transactions.flatMap((t) => t.labels),
-    ),
-  )
+  state.existingLabels = Array.from(new Set([...state.existingLabels, ...state.transactions.flatMap((t) => t.labels)]))
     .map(toTitleCase)
     .sort((a, b) => a.localeCompare(b));
 };
